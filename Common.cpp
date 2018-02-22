0a0798ae6d3c6317a2eb984d4416105ae9f4bb0c
Paul Meffle <meffle.paul@me.com>
2018-02-22 14:30:49 +0100

    Fix log string

--- Common.cpp
+++ Common.cpp
@@ -7,11 +7,11 @@ void logEvent(const Event& event)
         if (event.type == Event::Type::Connect)
         {
                 std::cout << "Connect";
-                std::cout << " from peer" << event.peer.id << std::endl;
+                std::cout << " from peer " << event.peer.id << std::endl;
         }
         else if (event.type == Event::Type::Disconnect)
         {
                 std::cout << "Disconnect";
-                std::cout << " from peer" << event.peer.id << std::endl;
+                std::cout << " from peer " << event.peer.id << std::endl;
         }
 }
