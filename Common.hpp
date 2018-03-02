cfe719f15ee48810e4104873e5ace6f8345f9774
Paul Meffle <meffle.paul@me.com>
2018-03-02 23:31:19 +0100

    Change client default port to any port available

--- Common.hpp
+++ Common.hpp
@@ -5,7 +5,7 @@
 #include <SFML/System/Time.hpp>
 #include <SFML/System/Vector2.hpp>
 
-constexpr Uint16 PORT_CL = 53535;
+constexpr Uint16 PORT_CL = 0;
 constexpr Uint16 PORT_SV = 42424;
 
 const sf::Time TIME_PER_TICK = sf::microseconds(50000);
